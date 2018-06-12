644439ea8d7eef377035a05614b9efdd952890bc
JohnnyMustang <jona.mustang@gmail.com>
2018-06-12 19:17:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,46 +8,46 @@ bot = commands.Bot(command_prefix='$')
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+	print('Logged in as')
+	print(bot.user.name)
+	print(bot.user.id)
+	print('------')
 
 class NamerBot(discord.Client):
-    def __init__(self, config_path: str, pokenames_path: str, *args, **kwargs):
-        self.config_path = config_path
-        with open(self.config_path) as f:
-            self.configs = json.load(f)
-        self.prefix = self.configs['command_prefix']
-        with open(pokenames_path, 'r', encoding='utf-8') as f:
-            self.pokenames = json.load(f)
-        super().__init__()
+	def __init__(self, config_path: str, pokenames_path: str, *args, **kwargs):
+		self.config_path = config_path
+		with open(self.config_path) as f:
+			self.configs = json.load(f)
+		self.prefix = self.configs['command_prefix']
+		with open(pokenames_path, 'r', encoding='utf-8') as f:
+			self.pokenames = json.load(f)
+		super().__init__()
 
-    def run(self):
-        super().run(self.configs['token'])    
+	def run(self):
+		super().run(self.configs['token'])    
 
-    async def match(self, url):
-        async with await self.sess.get(url) as resp:
-            dat = await resp.content.read()
-        m = hashlib.md5(dat).hexdigest()
-        return self.pokenames[m]
+	async def match(self, url):
+		async with await self.sess.get(url) as resp:
+			dat = await resp.content.read()
+		m = hashlib.md5(dat).hexdigest()
+		return self.pokenames[m]
 
-    async def on_message(self, message):
-        if message.author.id == 365975655608745985:
-            emb = message.embeds[0]
-            try:
-                embcheck = emb.title.startswith('A wild')
-            except AttributeError:
-                return    
-            if embcheck:
-                name = await self.match(emb.image.url.split('?')[0])
-                name = name.title()
-            await message.channel.send(name)
+	async def on_message(self, message):
+		if message.author.id == 365975655608745985:
+			emb = message.embeds[0]
+			try:
+			embcheck = emb.title.startswith('A wild')
+			except AttributeError:
+				return    
+			if embcheck:
+				name = await self.match(emb.image.url.split('?')[0])
+				name = name.title()
+			await message.channel.send(name)
 
-    async def on_ready(self):
-        print("NamerBot is online!")
+	async def on_ready(self):
+		print("NamerBot is online!")
 
-        self.ready = True
+		self.ready = True
 
 @bot.command()
 	async def add(ctx, a: int, b: int):
